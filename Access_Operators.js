21616cc0c9b488bbfa17f74a2724d46995e825fa
ReRu4 <150810028+ReRu4@users.noreply.github.com>
2024-12-22 08:41:37 +0300

    Update Access_Operators.js

--- Access_Operators.js
+++ Access_Operators.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Автоматизация настроек доступа с добавлением полей
 // @namespace    http://tampermonkey.net/
-// @version      1.0.2
+// @version      1.0.3
 // @description  Автоматический выбор названий и настройка доступа с добавлением полей
 // @author       ReRu (@Ruslan_Intertrade)
 // @match        *://leadvertex.ru/admin/callmodeNew/settings.html?category=6
@@ -32,7 +32,7 @@
         14: { group: "5", type: "1" },
         15: { group: "5", type: "2" },
     };
-    console.log("УРААААААААААААААА");
+    console.log("ФИНАЛ");
 
     if (location.href.includes("settings.html")) {
         const dialog = document.createElement('div');
