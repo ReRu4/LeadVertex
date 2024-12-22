68e7e3bc25c6418738f8744aaf4db22f659d51ca
ReRu4 <150810028+ReRu4@users.noreply.github.com>
2024-12-22 18:54:21 +0300

    Задержка на ОП 75

--- Access_Operators.js
+++ Access_Operators.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Автоматизация настроек доступа по АНГОЛЕ И АЛЖИРУ
 // @namespace    http://tampermonkey.net/
-// @version      1.2.3
+// @version      1.2.4
 // @description  Автоматический выбор названий и настройка доступа с добавлением полей
 // @author       ReRu (@Ruslan_Intertrade)
 // @match        *://leadvertex.ru/admin/callmodeNew/settings.html?category=6
